7b92fdfd79e0695aa1ce5e657c90a022fc8d3a78
Simonmiz <146436684+Simonmiz@users.noreply.github.com>
2024-04-02 15:53:31 +0200

    Update Patton.py

--- Scripts/Patton/Patton.py
+++ Scripts/Patton/Patton.py
@@ -1,85 +1,92 @@
-from time import sleep
-from tkinter import scrolledtext
-from paramiko import *
 import csv
 import datetime
 import os
 import shutil
+import configparser
+from time import sleep
+from paramiko import *
 
-# Jahreszhal variable
 datum = datetime.datetime.now().strftime("%m-%Y")
 now = datetime.datetime.now()
 
-ausgabe = r"T:\PATTON\Backups_Patton\Backup_Patton" + '-' + datum # Speicherort
+config = configparser.ConfigParser()
+config.read('../../config/config.ini')
+cg = config.get
 
-## Versucht einen Ordner zu erstellen
-## Falls dies scheitert Error message
+outputfile = cg("PATTON", "save_location")
+folder_name = cg("PATTON", "folder_name")
+list_csv = cg("GENERAL", "lists")
+delimiter = cg("GENERAL", "delimiter")
+
+outputfile = f"{outputfile}/{folder_name}_{datum}"
+
+list_csv = f"{list_csv}/patton.csv"
 
 try:
-    os.mkdir(ausgabe)
-    os.chmod(ausgabe, 0o777)
+    os.mkdir(outputfile)
+    os.chmod(outputfile, 0o777)
 except Exception as E1:
     print(now.strftime("%H:%M:%S") + ' Fehler beim erstellen des Ordners', E1)
 
-KI = r"C:\Users\Simon\Documents\Projekte\Backup_Machine\Script\_Subscripts\Patton\KI.csv" # Datei zum Auslesen
+try:
+    with open(list_csv, "r") as f:
+        reader = csv.reader(f, delimiter=";")
+        # Durchlaufe die ganze Liste
+        for row in reader:
 
-with open(KI, "r") as f:
+            # Speichere die Daten aus der aktuellen Zeile in Variablen
+            ip = row[0]
+            port = row[1]
+            user = row[2]
+            pw = row[3]
+            name = row[4]
 
-    # Erstelle einen Reader-Objekt
-    reader = csv.reader(f, delimiter=";")
+            customer = name
 
-    # Durchlaufe die ganze Liste
-    for row in reader:
+            SSH_USER = user
+            SSH_PASSWORD = pw
+            SSH_HOST = ip
+            SSH_PORT = port
+            client = SSHClient()
+            client.set_missing_host_key_policy(AutoAddPolicy())
 
-        # Speichere die Daten aus der aktuellen Zeile in Variablen
-        ip = row[0]
-        port = row[1]
-        user = row[2]
-        pw = row[3]
-        name = row[4]
-    
-        customername = name
-
-        SSH_USER = user
-        SSH_PASSWORD = pw
-        SSH_HOST = ip
-        SSH_PORT = port
-        client = SSHClient()
-        client.set_missing_host_key_policy(AutoAddPolicy())
-
-        now = datetime.datetime.now()
-        print(now.strftime("%H:%M:%S") + ' Verbindungsaufbau zu ' + SSH_HOST + ' wird hergestellt...')
-
-        try:
-            client.connect(SSH_HOST, port=SSH_PORT, username=SSH_USER, password=SSH_PASSWORD)
-        except Exception as E2:
-            print(now.strftime("%H:%M:%S") + ' Fehler beim Verbindungsaufbau', E2)
-            continue
-        channel = client.invoke_shell()
-
-        Backup_PT_CMD = "show running-config" # Config Writeout Befehl
-
-        channel.send(Backup_PT_CMD + "\n")
-        
-        sleep(10)
-
-        data = channel.recv(20480)
-        output = data
-            
-        output = output.decode("utf-8")
-
-        with open(customername +'-' + str(datum) +".cfg", "w") as out_file:
-            for line in output:
-                out_file.write(line)
-
-        # Ermittle den Pfad zur Datei "out_file"
-        out_file_path = os.path.abspath(out_file.name)
-
-        # Verschiebe die Datei "out_file" in den Ordner "technik"
-        try:
-            shutil.move(out_file_path, ausgabe)
             now = datetime.datetime.now()
-            print(now.strftime("%H:%M:%S") + ' Patton ' + ip + ' gesichert. (' + name +')')
-        except Exception as E3:
-            print(now.strftime("%H:%M:%S") + ' Fehler beim Verschieben der Datei:', E3)
-        channel.close()
+            print(now.strftime("%H:%M:%S") + ' Verbindungsaufbau zu ' + SSH_HOST + ' wird hergestellt...')
+
+            try:
+                client.connect(SSH_HOST, port=SSH_PORT, username=SSH_USER, password=SSH_PASSWORD)
+            except Exception as E2:
+                print(now.strftime("%H:%M:%S") + ' Fehler beim Verbindungsaufbau', E2)
+                continue
+            channel = client.invoke_shell()
+
+            Backup_PT_CMD = "show running-config"  # Config Print Befehl
+            channel.send(Backup_PT_CMD + "\n")
+
+            sleep(10)
+
+            data = channel.recv(20480)
+            output = data
+
+            output = output.decode("utf-8")
+
+            fn = customer + '-' + str(datum) + ".cfg"
+
+            with open(fn, "w") as out_file:
+                for line in output:
+                    out_file.write(line)
+
+            # Ermittle den Pfad zur Datei "out_file"
+            out_file_path = os.path.abspath(out_file.name)
+
+            # Verschiebe die Datei "out_file" in den Ordner "technik"
+            try:
+                shutil.move(fn, outputfile)
+                now = datetime.datetime.now()
+                print(now.strftime("%H:%M:%S") + ' Patton ' + ip + ' gesichert. (' + name + ')')
+            except Exception as E3:
+                print(now.strftime("%H:%M:%S") + ' Fehler beim Verschieben der Datei:', E3)
+            channel.close()
+except Exception as E4:
+    print(E4)
+sleep(5)
